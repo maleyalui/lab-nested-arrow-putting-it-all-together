23a44dc22d7feab8ba11557b28a6debe6e58af22
maleyalui <maleyalui1@gmail.com>
2025-12-02 14:23:06 +0300

    index.js

--- index.js
+++ index.js
@@ -4,3 +4,27 @@
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+function createLoginTracker(userInfo){
+  let attemptCount = 0;
+  let isLocked = false;
+
+  return (passwordAttempt) => {
+     //If you locked it always return the locked message
+     if (isLocked) {
+      return "Account locked due to too many failed login attempts";
+}
+     //correct password -> success
+     if (passwordAttempt === userInfo.password){
+      return"Login successful";
+}
+//wrong password
+attemptCount = attemptCount + 1;
+//if 3 failures -> lock account after returning the fail message
+if (attemptCount >= 3) {
+  isLocked = true;
+}
+
+return `Attempt ${attemptCount}: Login failed`;
+
+  };
+}
